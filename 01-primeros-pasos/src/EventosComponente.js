885106691e6d541fe5220a9c1901d956872f171b
marckes04 <maresp95@gmail.com>
2022-06-22 19:36:25 -0500

    first commit

--- 01-primeros-pasos/src/EventosComponente.js
+++ 01-primeros-pasos/src/EventosComponente.js
@@ -10,6 +10,17 @@ export const EventosComponente = () => {
         alert("Has dado doble click!!")
     }
 
+    const hasentrado = (e,accion) =>{
+        alert("Has"+accion+"entrado a la caja con el mouse!!!")
+    }
+
+    const estasDentro = e =>{
+        alert("Estas dentro del input, mete tu nombre!!");
+    }
+
+    const estasfuera = e =>{
+        alert("Estas fuera del input, chao");
+    }
     return (
     <div>
         <h1>Eventos en React</h1>
@@ -22,6 +33,20 @@ export const EventosComponente = () => {
         {/*Evento doble Click*/}
         <button onDoubleClick={HasDadoDobleclick}>Dame Doble Click!!</button>
         </p>
+
+        <div id='caja' 
+        onMouseEnter = {e => hasentrado(e,"entrado")}
+        onMouseLeave = {e => hasentrado(e,"salido")}>
+            {/* Evento OnMouseEnter onMouseLeave*/}
+            Pasa por encima
+        </div>
+            <p>
+                <input type = "text"
+                 onFocus={estasDentro} 
+                 onBlur ={estasfuera}
+                placeholder = "Introduce tu nombre......" />
+            </p>
+        
     </div>
   )
 }
